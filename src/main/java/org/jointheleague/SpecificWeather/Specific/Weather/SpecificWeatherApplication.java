346302e10348a9d1271d2c18501fee24869287bc
Aarit Jayaswal <aaritrj@gmail.com>
2024-12-28 10:58:18 -0800

    Initial Commit

---
+++ src/main/java/org/jointheleague/SpecificWeather/Specific/Weather/SpecificWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.SpecificWeather.Specific.Weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jointheleague/SpecificWeather/Specific/Weather/SpecificWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.SpecificWeather.Specific.Weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
